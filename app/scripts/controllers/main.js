656fb4a9bd5599a971809c19ddd2d6a886875f3c
Nicolas Hardy <nihar@smile.fr>
2016-02-12 00:19:00 +0100

    First commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name selogerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the selogerApp
+ */
+angular.module('selogerApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
